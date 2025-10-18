7a0821ffdc24b1e88687901199bcda32f8ae1acf
ender019 <gilkr125@gmail.com>
2025-10-18 14:05:53 +0300

    Инициализация проекта

---
+++ src/types/index.ts
@@ -0,0 +1,37 @@
+// src/types/index.ts
+export interface Cepheid {
+  id: string;
+  title: string;
+  img: string;
+  period: number;
+  source: string;
+  description?: string;
+  mv?: number;
+  asw?: number;
+  distance?: number;
+}
+
+export interface CepheidCalc {
+  id: string;
+  created_at: string;
+  ka: number;
+  kb: number;
+  items: Cepheid[];
+  approved_at?: string;
+  closed_at?: string;
+}
+
+export interface CepheidCalcListResponse {
+  id: string;
+  created_at: string;
+  ka: number;
+  kb: number;
+  status?: string;
+  approved_at?: string;
+  closed_at?: string;
+}
+
+export interface Breadcrumb {
+  label: string;
+  path?: string;
+}
