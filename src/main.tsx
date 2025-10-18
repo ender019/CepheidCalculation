09e65d167275b625fb5310bbaf45c013014601a4
ender019 <gilkr125@gmail.com>
2025-10-18 21:07:27 +0300

    Настройка SPA

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import {registerSW} from "virtual:pwa-register";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
+
+// Регистрация сервис-воркера
+if ('serviceWorker' in navigator) {
+  registerSW()
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,48 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 import react from '@vitejs/plugin-react'
+import mkcert from 'vite-plugin-mkcert'
+import fs from 'fs'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    mkcert(),
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: "Cepheid App",
+        short_name: "Cepheid",
+        start_url: "/RepoName/",
+        display: "standalone",
+        background_color: "#fdfdfd",
+        theme_color: "#db4938",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/logo192.png",
+            type: "image/png",
+            sizes: "192x192"
+          },
+          {
+            src: "/logo512.png",
+            type: "image/png", 
+            sizes: "512x512"
+          }
+        ],
+      },
+    }),
+  ],
+  base: "/RepoName/", 
   server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, 'cert.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, 'cert.crt')),
+    },
     proxy: {
       '/api/v1': {
         target: 'http://localhost:8080',
