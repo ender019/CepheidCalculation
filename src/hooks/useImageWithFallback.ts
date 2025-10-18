9e01c49491738d29e4469365277d5c2b182d887d
ender019 <gilkr125@gmail.com>
2025-10-18 14:09:01 +0300

    Хук для загрузки изображений

---
+++ src/hooks/useImageWithFallback.ts
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+export const useImageWithFallback = (initialSrc: string, fallbackSrc: string = '/img/default-star.jpg') => {
+  const [src, setSrc] = useState(initialSrc);
+  const [hasError, setHasError] = useState(false);
+
+  useEffect(() => {
+    console.log(initialSrc)
+    setSrc('/static'+initialSrc);
+    setHasError(false);
+  }, [initialSrc]);
+
+  const handleError = () => {
+    if (!hasError) {
+      setSrc(fallbackSrc);
+      setHasError(true);
+    }
+  };
+
+  const handleLoad = () => {
+    setHasError(false);
+  };
+
+  return {
+    src,
+    hasError,
+    onError: handleError,
+    onLoad: handleLoad
+  };
+};
