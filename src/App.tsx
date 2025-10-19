46c6f1b1e54e7e7fe88558f67cf3d3624824b611
ender019 <gilkr125@gmail.com>
2025-10-19 21:42:18 +0300

    tauru

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,23 @@ import Navbar from './components/Navbar';
 import HomePage from './pages/HomePage';
 import CepheidsPage from './pages/CepheidsPage';
 import CepheidDetailPage from './pages/CepheidDetailPage';
+import { useEffect } from 'react';
 import { ROUTES } from './Routes';
 
+import { invoke } from "@tauri-apps/api/core";
+
 function App() {
+  useEffect(()=>{
+    invoke('tauri', {cmd:'create'})
+      .then(() =>{console.log("Tauri launched")})
+      .catch(() =>{console.log("Tauri not launched")})
+    return () =>{
+      invoke('tauri', {cmd:'close'})
+        .then(() =>{console.log("Tauri launched")})
+        .catch(() =>{console.log("Tauri not launched")})
+    }
+  }, [])
+
   return (
     <BrowserRouter basename='/Gilyazetdinov-RIP2025F'>
       <div className="App">
