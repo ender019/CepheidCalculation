5fee1e6c49432db9eb97b8be3f39d7add99c6422
ender019 <gilkr125@gmail.com>
2025-12-16 17:41:11 +0300

    fix

--- src/hooks/useImageWithFallback.ts
+++ src/hooks/useImageWithFallback.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-const host = "https://192.168.43.245:3000/Gilyazetdinov-RIP2025F"
+const host = "https://192.168.43.245:3000"
 
 export const useImageWithFallback = (initialSrc: string, fallbackSrc: string = '/Gilyazetdinov-RIP2025F/img/default-star.jpg') => {
   const [src, setSrc] = useState(initialSrc);
